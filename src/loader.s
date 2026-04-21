de1819c6d112edc65ab24cf2a3efbd8fbff0c0c2
Vincent S. <vs38190@gmail.com>
2026-04-21 02:13:46 +0200

    [UPDATE] Refactor loader and main functions for dynamic DLL loading

--- src/loader.s
+++ src/loader.s
@@ -1,51 +1,47 @@
 use64
 
-; This is a raw binary file containing the machine code
 
 ; Expecting
 ; - RCX: LoadLibraryA
 ; - RDX: GetProcAddress
-; - R8: lpText
-; - R9: lpCaption
+; - R8: lpDllPath
+; - R9: lpEntryPoint
 _greet:
     ; [0-32[     : for stack alignment
     ; [32]       : for LoadLibraryA
     ; [40]       : for GetProcAddress
-    ; [48]       : for lpText
-    ; [56]       : for lpCaption
+    ; [48]       : for lpDllPath
+    ; [56]       : for lpEntryPoint
     sub rsp, 8*9
 
     LoadLibraryA   = 8*4 ; 32
     GetProcAddress = 8*5 ; 40
-    lpText         = 8*6 ; 48
-    lpCaption      = 8*7 ; 56
-
+    lpDllPath      = 8*6 ; 48
+    lpEntryPoint   = 8*7 ; 56
+    
     mov [rsp + LoadLibraryA], rcx   ; Save LoadLibraryA
     mov [rsp + GetProcAddress], rdx ; Save GetProcAddress
-    mov [rsp + lpText], r8          ; Save lpText
-    mov [rsp + lpCaption], r9       ; Save lpCaption
+    mov [rsp + lpDllPath], r8        ; Save lpDllPath
+    mov [rsp + lpEntryPoint], r9     ; Save lpEntryPoint
 
-    ; Load user32.dll
-    lea rcx, [user_name]          ; lpLoadLibraryA
-    mov rax, [rsp + LoadLibraryA] ; LoadLibraryA
+    ; Load dll from lpDllPath
+    mov rcx, [rsp + lpDllPath]     ; lpDllPath
+    mov rax, [rsp + LoadLibraryA]  ; LoadLibraryA
     call rax
     test rax, rax
     jz bad_module_load ; If failed to load user32.dll, exit early
 
-    ; Get MessageBoxA address
+    ; Get address of lpEntryPoint
     mov rcx, rax                    ; hModule
-    lea rdx, [msg_box_a_name]       ; lpProcName
+    mov rdx, [rsp + lpEntryPoint]   ; lpProcName
     mov rax, [rsp + GetProcAddress] ; GetProcAddress
     call rax                        ; call GetProcAddress
     test rax, rax
-    jz bad_proc_load ; If failed to get MessageBoxA address, exit early
+    jz bad_proc_load ; If failed to get lpEntryPoint address, exit early
 
-    ; Call MessageBoxA
-    mov rcx, 0                      ; hWnd
-    mov rdx, [rsp + lpText]         ; lpText
-    mov r8, [rsp + lpCaption]       ; lpCaption
-    mov r9, 0                       ; uType (MB_OK)
-    call rax                        ; call MessageBoxA
+    ; Call lpEntryPoint    
+    ; todo - we should pass pointer to the arguments struct here
+    call rax ; call lpEntryPoint
 
     mov rax, 0 ; exit code 0
     jmp done
@@ -57,6 +53,3 @@ bad_proc_load:
 done:
     add rsp, 8*9 ; Restore stack
     ret
-
-user_name db 'USER32.DLL',0
-msg_box_a_name db 'MessageBoxA',0
--- src/main.zig
+++ src/main.zig
@@ -9,30 +9,19 @@ extern "kernel32" fn FreeLibrary(win.HMODULE) callconv(.winapi) win.BOOL;
 extern "kernel32" fn VirtualAlloc(?win.LPVOID, win.SIZE_T, win.DWORD, win.DWORD) callconv(.winapi) ?win.LPVOID;
 extern "kernel32" fn VirtualFree(win.LPVOID, win.SIZE_T, win.DWORD) callconv(.winapi) win.BOOL;
 extern "kernel32" fn VirtualProtect(win.LPVOID, win.SIZE_T, win.DWORD, *win.DWORD) callconv(.winapi) win.BOOL;
-extern "user32" fn MessageBoxA(?win.HWND, [*:0]const u8, [*:0]const u8, u32) callconv(.winapi) i32;
 
 const LoadLibraryFn = *const fn ([*:0]const u8) callconv(.winapi) ?win.HMODULE;
 const GetProcAddressFn = *const fn (win.HMODULE, [*:0]const u8) callconv(.winapi) ?win.FARPROC;
 const MessageBoxAFn = *const fn (?win.HWND, [*:0]const u8, [*:0]const u8, u32) callconv(.winapi) i32;
 
-const GreetFn = *const fn (LoadLibraryFn, GetProcAddressFn, [*:0]const u8, [*:0]const u8) callconv(.winapi) i32;
-const GreetEasyFn = *const fn (MessageBoxAFn, [*:0]const u8, [*:0]const u8) callconv(.winapi) i32;
+fn run(dllPath: [*:0]const u8, entryPoint: [*:0]const u8) !void {
+    const LoaderFn = *const fn (LoadLibraryFn, GetProcAddressFn, [*:0]const u8, [*:0]const u8) callconv(.winapi) i32;
 
-const Args = struct {
-    loadLibrary: LoadLibraryFn,
-    getProcAddress: GetProcAddressFn,
-};
-
-fn runHello() !void {
-    // const PAGE_EXECUTE_READWRITE: win.DWORD = 0x40;
     const PAGE_EXECUTE_READ: win.DWORD = 0x20;
     const PAGE_READWRITE: win.DWORD = 0x04;
     const MEM_COMMIT: win.DWORD = 0x1000;
     const MEM_RESERVE: win.DWORD = 0x2000;
     const MEM_RELEASE: win.DWORD = 0x8000;
-
-    // Compute sayHello's size by taking the difference between its address and
-    // the address of a sentinel function defined immediately after it.
 
     const rawData = @embedFile("payload_bin");
     log.debug("Raw data length: {d}", .{rawData.len});
@@ -54,37 +43,20 @@ fn runHello() !void {
     }
 
     // Call the function from its new location.
-    const fnPtr: GreetFn = @ptrCast(mem);
-    // const fnPtr: GreetEasyFn = @ptrCast(mem);
+    const fnPtr: LoaderFn = @ptrCast(mem);
 
-    const ret = fnPtr(&LoadLibraryA, &GetProcAddress, "Hello from the hijacked function!", "Hijack");
-    // const ret = fnPtr(&MessageBoxA, "Hello from the hijacked function!", "Hijack");
+    const ret = fnPtr(&LoadLibraryA, &GetProcAddress, dllPath, entryPoint);
 
-    log.info("Greet function returned: {d}", .{ret});
+    log.debug("Function {s} returned: {d}", .{ entryPoint, ret });
 }
 
 pub fn main(init: std.process.Init) !void {
     _ = init;
 
+    const dllPath = "zig-out\\bin\\lib.dll";
+    const entryPoint = "displayBox";
+
     log.info("Hello, World!", .{});
 
-    // try loadDllAndRun();
-
-    try runHello();
-}
-
-pub fn loadDllAndRun() !void {
-    const hModule = LoadLibraryA("zig-out\\bin\\lib.dll") orelse {
-        return error.LoadLibraryFailed;
-    };
-    defer {
-        _ = FreeLibrary(hModule);
-    }
-
-    const rawProc = GetProcAddress(hModule, "displayBox") orelse {
-        return error.GetProcAddressFailed;
-    };
-    const entrypoint = @as(*const fn () void, @ptrCast(rawProc));
-
-    entrypoint();
+    try run(dllPath, entryPoint);
 }
